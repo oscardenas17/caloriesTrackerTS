30aa02884b80bb96371ade85cca60b3c989a84ef
oscardenas <oscardenas17@gmail.com>
2024-04-01 12:08:32 -0500

    Generando ids para las actividades

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
 import { Dispatch, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import { categories } from "../data/categories";
 import type { Activity } from "../types";
 import { ActivityActions } from "../reducers/activity-reducer";
@@ -7,12 +8,15 @@ type FormProps = {
   dispatch: Dispatch<ActivityActions>;
 };
 
+const initialState: Activity = {
+  id: uuidv4(),
+  category: 1,
+  name: "",
+  calories: 0,
+};
+
 const Form = ({ dispatch }: FormProps) => {
-  const [activity, setActivity] = useState<Activity>({
-    category: 1,
-    name: "",
-    calories: 0,
-  });
+  const [activity, setActivity] = useState<Activity>(initialState);
 
   const handleChange = (
     e:
@@ -34,7 +38,7 @@ const Form = ({ dispatch }: FormProps) => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     dispatch({ type: "save-activity", payload: { newActivity: activity } });
-    setActivity({ category: 1, name: "", calories: 0 });
+    setActivity({ ...initialState, id: uuidv4() });
   };
 
   return (
