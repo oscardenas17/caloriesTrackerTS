d48def23643ccacd37ddbcd7d44176bead742599
oscardenas <oscardenas17@gmail.com>
2024-04-01 12:54:22 -0500

    Creando componente listar actividades y comidas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useReducer } from "react";
 import Form from "./components/Form";
 import { activityReducer, initialState } from "./reducers/activity-reducer";
+import ActivityList from "./components/ActivityList";
 
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState);
@@ -22,6 +23,10 @@ function App() {
         <div className="max-w-4xl mx-auto">
           <Form dispatch={dispatch} />
         </div>
+      </section>
+
+      <section className="p-10 mx-auto max-w-4xl">
+        <ActivityList activities={state.activities} />
       </section>
     </>
   );
