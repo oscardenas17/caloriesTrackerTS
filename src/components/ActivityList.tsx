a19bc32c2ce726b79d5bf7cc14379be2f711348d
oscardenas <oscardenas17@gmail.com>
2024-04-01 13:23:23 -0500

    Listado de atividades

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,10 +1,17 @@
+import { useMemo } from "react";
 import type { Activity } from "../types";
+import { categories } from "../data/categories";
 
 type ActivityListProps = {
   activities: Activity[];
 };
 
 const ActivityList = ({ activities }: ActivityListProps) => {
+  const categoryName = useMemo(
+    () => (category: Activity["category"]) =>
+      categories.map((cat) => (cat.id === category ? cat.name : "")),
+    [activities]
+  );
   return (
     <>
       <h2 className="text-4xl font-bold text-slate-600 text-center">
@@ -16,9 +23,15 @@ const ActivityList = ({ activities }: ActivityListProps) => {
           key={activity.id}
           className="px-5 py-10 bg-white mt-5 flex justify-between"
         >
-          <div className="sáce-y-2 relative">
+          <div className="space-y-2 relative">
             {" "}
-            <p>{activity.category}</p>
+            <p
+              className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${
+                activity.category === 1 ? "bg-lime-500" : "bg-orange-500"
+              }`}
+            >
+              {categoryName(+activity.category)}
+            </p>
             <p className="text-2xl font-bold pt-5">{activity.name}</p>
             <p className="font-black text-4xl text-lime-500">
               {activity.calories} <span>Calorias</span>
